5154867965530f7e568cacb584296dbb1d6b77c8
hello112334 <hello112334@gmail.com>
2024-09-05 15:53:32 +0900

    fix to auto redirect

--- ios/myShareExtension/ShareViewController.swift
+++ ios/myShareExtension/ShareViewController.swift
@@ -11,7 +11,12 @@ class ShareViewController: RSIShareViewController {
     // Use this method to return false if you don't want to redirect to host app automatically.
     // Default is true
     override func shouldAutoRedirect() -> Bool {
-        return false
+        return true
     }
     
+    // Use this to change label of Post button
+    // override func presentationAnimationDidFinish() {
+    //     super.presentationAnimationDidFinish()
+    //     navigationController?.navigationBar.topItem?.rightBarButtonItem?.title = "Send"
+    // }
 }
