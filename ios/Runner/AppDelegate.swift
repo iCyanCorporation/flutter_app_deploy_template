bf63570d826a89b1ee7a6b567cc65ee534a1983b
hello112334 <hello112334@gmail.com>
2024-11-01 15:31:39 +0900

    fix appdelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,31 +1,13 @@
-import UIKit
 import Flutter
+import UIKit
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let controller = window?.rootViewController as! FlutterViewController
-    let methodChannel = FlutterMethodChannel(name: "com.icyan.todolist/share",
-                                             binaryMessenger: controller.binaryMessenger)
-
-    methodChannel.setMethodCallHandler { (call, result) in
-      if call.method == "getSharedText" {
-        // Retrieve shared text from UserDefaults (or other shared data sources)
-        let userDefaults = UserDefaults(suiteName: "group.com.todolist.shareExtension")
-        userDefaults?.set("Shared text from LINE", forKey: "sharedText")
-        if let sharedText = userDefaults?.string(forKey: "sharedText") {
-          result(sharedText)
-        } else {
-          result(FlutterError(code: "NO_DATA", message: "No shared text found", details: nil))
-        }
-      } else {
-        result(FlutterMethodNotImplemented)
-      }
-    }
-
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
