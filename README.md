0d1d7ecbdacfc54eb9a5190f218d988c57c2a6eb
hello112334 <37241401+hello112334@users.noreply.github.com>
2024-11-01 17:59:50 +0900

    Dev (#1)
    * init
    
    * init
    
    * add share extension
    
    * pod setup
    
    * update
    
    * update view

--- README.md
+++ README.md
@@ -14,3 +14,21 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## step
+### 1
+```
+flutter create .
+```
+
+### 2
+1. add share extension by xcode in mac.
+2. add app group
+
+### 3
+https://github.com/KasemJaffer/receive_sharing_intent
+
+### 4
+```
+pod install
+```
--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,18 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <amplify_db_common/amplify_db_common_plugin.h>
-#include <mecab_dart/mecab_dart_plugin.h>
-#include <url_launcher_linux/url_launcher_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
-  g_autoptr(FlPluginRegistrar) amplify_db_common_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "AmplifyDbCommonPlugin");
-  amplify_db_common_plugin_register_with_registrar(amplify_db_common_registrar);
-  g_autoptr(FlPluginRegistrar) mecab_dart_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "MecabDartPlugin");
-  mecab_dart_plugin_register_with_registrar(mecab_dart_registrar);
-  g_autoptr(FlPluginRegistrar) url_launcher_linux_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "UrlLauncherPlugin");
-  url_launcher_plugin_register_with_registrar(url_launcher_linux_registrar);
 }
