bdf37f77cac4b4d1766c61c124cc89bc84e508a1
Toyofumi <hello112334@gmail.com>
2024-11-01 16:20:38 +0900

    init

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,18 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <amplify_db_common/amplify_db_common_plugin.h>
-#include <connectivity_plus/connectivity_plus_windows_plugin.h>
-#include <mecab_dart/mecab_dart_plugin_c_api.h>
-#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  AmplifyDbCommonPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("AmplifyDbCommonPlugin"));
-  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
-  MecabDartPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("MecabDartPluginCApi"));
-  UrlLauncherWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
